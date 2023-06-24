1600443135c3d9cf484132026c5102eb527d5448
Stephan <s.dumenko@icloud.com>
2023-06-24 08:47:50 +0200

    fix: bug fixed

--- src/js/classes/VisitCardiologist.js
+++ src/js/classes/VisitCardiologist.js
@@ -1,2 +1,12 @@
 import Visit from "./Visit.js";
-export default class VisitCardiologist extends Visit {}
+export default class VisitCardiologist extends Visit {
+    constructor() {
+        super();
+        this.doctor = document.createElement("div");
+    }
+
+    createElements() {
+        super();
+        this.doctor.className
+    }
+}
--- src/js/script.js
+++ src/js/script.js
@@ -5,14 +5,14 @@ import CardHtml from './classes/cardHtml.js';
 const cardContainer = document.querySelector(".reserwation__card-container");
 
 
-  // const modal = new Modal();
-  // const modalContainer = document.getElementById('modalContainer');
-  // modalContainer.appendChild(modal.render());
+  const modal = new Modal();
+  const modalContainer = document.getElementById('modalContainer');
+  modalContainer.appendChild(modal.render());
 
-    // const visit = new Visit();
-    // const visitFormContainer = document.getElementById('visitForm');
-    // const visitFormHTML = visit.render().outerHTML;
-    // visitFormContainer.innerHTML = visitFormHTML;
+    const visit = new Visit();
+    const visitFormContainer = document.getElementById('visitForm');
+    const visitFormHTML = visit.render().outerHTML;
+    visitFormContainer.innerHTML = visitFormHTML;
 
 new CardHtml('Sick', 'lkdafldkmsldkfmlsmf', 'High', 'Jhon Doe', 'Stan Smith').render();
 new CardHtml('Sick', 'lkdafldkmsldkfmlsmf', 'High', 'Jhon Doe', 'Stan Smith').render();
