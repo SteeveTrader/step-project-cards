a6a4865382a5881e11f92a8027f19e2cf1a4220f
TimoMax <max.timoshenko@gmail.com>
2023-06-26 20:08:22 +0300

    some items fixed

--- src/js/API/removeCard.js
+++ src/js/API/removeCard.js
@@ -1,21 +1,21 @@
-import { urlCards, TOKEN } from "./variables.js";
+import { urlCards } from "./variables.js";
 
 // Видалення усіх карток
 export async function deleteAllCards() {
   try {
-    // Отримуємо список всіх карточок
+    
     const response = await axios.get(urlCards, {
       headers: {
-        'Authorization': Bearer ${localStorage.getItem('tokenKey')}
+        'Authorization': Bearer `${localStorage.getItem('tokenKey')}`
       }
     });
 
     const cards = response.data;
 
     for (const card of cards) {
-      await axios.delete(${urlCards}/${card.id}, {
+      await axios.delete(`${urlCards}/${card.id}`, {
         headers: {
-          'Authorization': Bearer ${TOKEN}
+          'Authorization': Bearer`${localStorage.getItem('tokenKey') }`
         }
       });
     }
