0f8c44d22dacdcc9cd14748de568bd5ce98cfeab
Stephan <s.dumenko@icloud.com>
2023-07-04 23:28:55 +0200

    fix serverrequest

--- src/js/API/serverRequest.js
+++ src/js/API/serverRequest.js
@@ -1,42 +1,42 @@
 
 import instance from "./instance.js";
 
-// export const fetchData = async () => {  
-//   try {
-
-//     return await instance.get("/");
-//   } catch(err) {
-//     console.log('Error', err);
-// }
-
-// };
-
-
-
-
-
-
-
-
-
-
-export const fetchData = async () => {
+export const fetchData = async () => {  
   try {
-    const response = await instance.get("/");
 
-    if (response.data.length === 0) {
-      const container = document.querySelector('.reserwation');
-      const div = document.createElement('div');
-      div.classList.add('message');
-      div.innerText = "Не має визитів";
-      container.append(div);
-
-      return await instance.get("/");
-    } else {
-      console.log("все ок");
-      return response;
-    }
-  } catch (err) {
+    return await instance.get("/");
+  } catch(err) {
     console.log('Error', err);
-  }
+}
+
 };
+
+
+
+
+
+
+
+
+
+
+// export const fetchData = async () => {
+//   try {
+//     const response = await instance.get("/");
+
+//     if (response.data.length === 0) {
+//       const container = document.querySelector('.reserwation');
+//       const div = document.createElement('div');
+//       div.classList.add('message');
+//       div.innerText = "Не має визитів";
+//       container.append(div);
+
+//       return await instance.get("/");
+//     } else {
+//       console.log("все ок");
+//       return response;
+//     }
+//   } catch (err) {
+//     console.log('Error', err);
+//   }
+// };
