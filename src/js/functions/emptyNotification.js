7d50a568fcc93b81a875682d41dfc9ba4085d34a
TimoMax <max.timoshenko@gmail.com>
2023-07-05 19:13:17 +0300

    migrate changes 13 to 14 branches

---
+++ src/js/functions/emptyNotification.js
@@ -0,0 +1,8 @@
+import CardsData from "./cardsData.js";
+CardsData;
+
+export default function emptyNotification() {
+
+  const notificationEmptyArray = document.querySelector(".reserwation__container-empty-array")
+  CardsData.length === 0 ? notificationEmptyArray.style.display = "block" : notificationEmptyArray.style.display = "none";
+}
